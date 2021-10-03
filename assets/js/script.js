0d5d4fdef7a8e30583ffd376f1f855bd792298d2
Brent Graves <graves.brent@gmail.com>
2021-10-03 18:29:27 -0400

    added variables for searchHistoryBtn.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,14 @@ var forcastEl = document.getElementById("forcast")
 let forcastBlock = document.createElement("div")
 let colorBlock = document.querySelector(".color")
 
+searchHistoryBtn();
+let appDate = function (time) {
+    let displayDate = new Date();
+    displayDate.setTime(time * 1000);
+    let dd = displayDate.getDate();
+    let mm = displayDate.getMonth() + 1;
+    let y = displayDate.getFullYear();
+    return mm + '/' + dd + '/' + y;
+}
+
 var APIKey = "3a86af078e820c30e7a3322768448284";
