579c741823eda67f235af7dd173c37b900a7781f
Brent Graves <graves.brent@gmail.com>
2021-10-03 18:10:14 -0400

    added the dom element references for js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
+//dom element refs
+var searchBtnEl = document.getElementById("searchBtn")
+var searchCityEl = document.getElementById("searchCity")
+var boldDataEl = document.getElementById("boldData")
+var tempMainEl = document.getElementById("tempMain")
+var windMainEl = document.getElementById("windMain")
+var humidMainEl = document.getElementById("humidMain")
+var uvMainEl = document.getElementById("uvMain")
+var forcastEl = document.getElementById("forcast")
+let forcastBlock = document.createElement("div")
+let colorBlock = document.querySelector(".color")
+
 var APIKey = "3a86af078e820c30e7a3322768448284";
